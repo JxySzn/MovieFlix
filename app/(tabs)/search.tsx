0645a52d4b063bb071f4e50c60f119f292e61d55
Jason <byronjason902@gmail.com>
2025-08-19 09:04:12 +0100

    first commit

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const search = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-700 text-7xl">search</Text>
+    </View>
+  );
+};
+
+export default search;
